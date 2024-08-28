09b373daa7b9e25b5fe72c06c87d039e7c916f2d
enesyanal <enesdnbd@gmail.com>
2024-08-28 19:11:44 +0300

    Add files via upload

---
+++ kitap_proje.sql
@@ -0,0 +1,11 @@
+INSERT INTO kitaplar (kitap_ismi, yazar, yayin_yili) VALUES
+('Saatleri Ayarlama Enstitüsü', 'Ahmet Hamdi Tanpınar', 1961),
+('Tutunamayanlar', 'Oğuz Atay', 1971),
+('İnce Memed', 'Yaşar Kemal', 1955),
+('Aşk', 'Elif Şafak', 2009),
+('Beyaz Kale', 'Orhan Pamuk', 1985),
+('Eylül', 'Mehmet Rauf', 1900),
+('Sinekli Bakkal', 'Halide Edib Adıvar', 1935),
+('Çalıkuşu', 'Reşat Nuri Güntekin', 1922);
+
+SELECT * FROM kitaplar;
